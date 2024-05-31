39612c808ba12bd0e9531a839e4289944c912478
amanshridev <mailtoamanshri@proton.me>
2024-05-31 20:56:25 +0530

    JavaScript Refresher

---
+++ 01intro.js
@@ -0,0 +1,19 @@
+/*
+
+What is JavaScript ?
+JavaScript is a scripting language that allow users to implement tasks and features on the web.
+It also help in adding extra dynamic features like animation, updating or in manipulating any
+kind of data.
+
+
+How Dose JavaScript Works?
+JavaScript engine help the system to understand the javascript code/ program. Most browser has this
+js engin built in there system. 
+
+Example: 
+
+*/
+
+console.log("Setup Complete");
+
+
---
+++ 02variables.js
@@ -0,0 +1,4 @@
+/*
+Variables in JavaScript: 
+They are like containers that holds the value. Value can be of any kind like numbers, strings, boolen etc. 
+*/
