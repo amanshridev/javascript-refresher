1e097132f0f43bb1dc144411dffb573941ba42a5
amanshridev <mailtoamanshri@proton.me>
2024-05-31 21:01:32 +0530

    Variables

--- 02variables.js
+++ 02variables.js
@@ -1,4 +1,11 @@
 /*
 Variables in JavaScript: 
 They are like containers that holds the value. Value can be of any kind like numbers, strings, boolen etc. 
+
 */
+
+//Var Example
+
+var firstName = "Name";
+
+console.log(firstName)
