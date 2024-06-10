5eb784947cafb4dd2991aa4c98532f7e88af9027
Aman Shrivastav <130833600+amanshridev@users.noreply.github.com>
2024-06-10 18:27:12 +0000

    about array

--- array.js
+++ array.js
@@ -1,1 +1,11 @@
 console.log("Arrays in js");
+
+/*
+
+The Array object, as with arrays in other programming languages, enables storing a collection 
+of multiple items under a single variable name, and has members for performing common array 
+operations.
+
+In Js Arrays are non-primitive type of data.
+
+*/
