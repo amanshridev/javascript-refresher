e6faa870759791c3673615748f2bd19dcadb56d6
Aman Shrivastav <130833600+amanshridev@users.noreply.github.com>
2024-06-16 11:05:39 +0000

    while

---
+++ while.js
@@ -0,0 +1,36 @@
+//While
+
+let num = 1
+ 
+while (num < 6) {
+    // console.log(num)
+    num++
+}
+
+//while in array
+
+let nums = ['one', 'two', 'three'];
+
+let arr = 0
+
+while( arr < nums.length ){
+    // console.log(nums[arr])
+    arr++
+}
+
+//do while
+
+let num2 = 0
+
+do {
+    // console.log(num2)
+    num2++
+} while ( num2 < 11);
+
+
+let num3 = 20
+
+do { //in do while loop even if the condition not matches the loop will run for a single time
+    console.log(num3)
+    num3++
+} while ( num2 < 11);
