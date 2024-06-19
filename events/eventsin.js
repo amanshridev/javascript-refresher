8b5a6a018627bae1599eefaa9c28de9a8d1f2a10
Aman Shrivastav <130833600+amanshridev@users.noreply.github.com>
2024-06-19 18:03:05 +0000

    Event Listner

---
+++ events/eventsin.js
@@ -0,0 +1,12 @@
+
+/* 
+
+An event listener in JavaScript is a function that waits for a specific event to occur
+like clicking a button, and then responds to it. like on clicking a button and something happened. 
+When an event occurs, the event listener is triggered and the function it contains is executed.
+
+*/
+
+document.getElementById('naruto').addEventListener('click', function(){
+    alert('Naruto Boiii')
+})
